f23b97b396b223142d3094f00259b5bfbe2b599e
The Chief <7783091+chiafuliom@users.noreply.github.com>
2026-03-18 13:17:43 -0400

    Update AmmoPickup.cs

--- AmmoPickup.cs
+++ AmmoPickup.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
-    public AudioSource AmmoSound; //sound effect of picking up the ammo crate
-    void OnTriggerEnter(Collider other) //when this triggered box collider makes contact with the player...
-    {
-        AmmoSound.Play();//play the sound FX of picking up the crate
+    public AudioClip AmmoClip; // drag the sound file here instead
 
-        if (GlobalAmmo.LoadedAmmo == 0)//if the bullets in the first display reach zero...
+    void OnTriggerEnter(Collider other)
+    {
+        AudioSource.PlayClipAtPoint(AmmoClip, transform.position);
+
+        if (GlobalAmmo.LoadedAmmo == 0)
         {
-            GlobalAmmo.LoadedAmmo += 10;//add ten to the first display
-            this.gameObject.SetActive(false);//make the crate disappear
+            GlobalAmmo.LoadedAmmo += 10;
         }
         else
         {
-            GlobalAmmo.CurrentAmmo += 10;//otherwise add 10 to the reserve ammo in the second display
-            this.gameObject.SetActive(false);//make the crate disappear
+            GlobalAmmo.CurrentAmmo += 10;
         }
+
+        this.gameObject.SetActive(false);
     }
 }
 
