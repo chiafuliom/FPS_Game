77a3c8c9d4664c08546c03ca361eb3302694acf7
The Chief <7783091+chiafuliom@users.noreply.github.com>
2026-03-18 13:15:21 -0400

    Update GlobalAmmo.cs

--- GlobalAmmo.cs
+++ GlobalAmmo.cs
@@ -1,25 +1,17 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GlobalAmmo : MonoBehaviour
 {
-    public static int CurrentAmmo;
-    public int InternalAmmo;
-    public GameObject AmmoDisplay;
+    public static int CurrentAmmo = 0;
+    public static int LoadedAmmo = 10;
 
-    public static int LoadedAmmo;
-    public int InternalLoaded;
-    public GameObject LoadedDisplay;
-
+    public Text AmmoDisplay;
+    public Text LoadedDisplay;
 
     void Update()
     {
-        InternalAmmo = CurrentAmmo;
-
-        InternalLoaded = LoadedAmmo;
-        AmmoDisplay.GetComponent<Text>().text = "" + InternalAmmo;
-        LoadedDisplay.GetComponent<Text>().text = "" + LoadedAmmo;
+        AmmoDisplay.text = CurrentAmmo.ToString();
+        LoadedDisplay.text = LoadedAmmo.ToString();
     }
 }
